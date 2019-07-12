488f03995b7272797202ca115baefadda5f2855c
Abakumov <abakumov_ivan@mail.ru>
2019-07-12 13:48:30 +0200

    adds MLIB library and subfoders to the MATLAB path

---
+++ add_mlib_path.m
@@ -0,0 +1,9 @@
+%% ADD_MLIB_PATH.m
+% adds MLIB library and its subfolders to the MATLAB search path 
+%
+%% Author: Ivan Abakumov
+% Freie Universität Berlin
+% E-mail: abakumov_ivan@mail.ru
+% Publication date: 12th July 2019
+
+addpath(genpath(mlibfolder))
