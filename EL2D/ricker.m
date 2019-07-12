cef7e8f6a2734f9ebd860659b9ba0f8b2982e657
Abakumov <abakumov_ivan@mail.ru>
2019-07-12 14:41:46 +0200

    RICKER wavelet

---
+++ EL2D/ricker.m
@@ -0,0 +1,11 @@
+function a = ricker(fpeak,t)
+%RICKER 
+% Ricker wavelet in time
+% fpeak is peak frequency in Hz
+% t is time array
+    tcut = 1.5/fpeak;    
+    s = (t-tcut).*fpeak;
+    a = (1-2*pi^2*s.^2).*exp(-pi^2*s.^2);
+    a(abs(s)>4.) = 0;    
+end
+
